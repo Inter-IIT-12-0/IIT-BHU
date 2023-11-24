f1790fbf07610a9af1b322562a04a4c4029e69db
Roaster05 <100672872+Roaster05@users.noreply.github.com>
2023-11-24 22:52:15 +0530

    API Documentation added

--- src/pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-import connectDb from "../../../middlewares/mongoose.js";
-
-const handler = (req, res) => {
-    console.log("Hello");
-    res.status(200).json({ message: "Hello, Next.js API!" });
-};
-
-export default connectDb(handler);
---
+++ src/pages/api/user/hello.js
@@ -0,0 +1,6 @@
+// pages/api/hello.js
+export default function handler(req, res) {
+    // This API endpoint responds with a JSON object
+    res.status(200).json({ message: 'Hello, this is a basic API in Next.js!' });
+  }
+  
