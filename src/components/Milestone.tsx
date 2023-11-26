90b042e5c227346fb592b42a20abb88390a39f6b
chiggshiggs <chirag.goel.mat21@itbhu.ac.in>
2023-11-27 03:17:03 +0530

    added color scheme to milestone

--- src/components/Milestone.tsx
+++ src/components/Milestone.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { useState } from "react";
+import milestones from '../../models/Milestone.json';
 
-interface MyComponentProps {
-  // Define your component props here
+interface MilestoneProps {
+  subMilestones: { heading: string; isCompleted: boolean }[];
 }
 
-const Milestone: React.FC<MyComponentProps> = () => {
+const Milestone: React.FC<MilestoneProps> = ({ subMilestones }) => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
 
@@ -14,19 +15,7 @@ const Milestone: React.FC<MyComponentProps> = () => {
     setHoveredIndex(index);
   };
 
-  const milestones = [
-    {
-      title: 'Application UI code in Tailwind CSS',
-      date: 'February 2022',
-      additionalContent: 'This is the first component.',
-    },
-    {
-      title: 'Another Milestone',
-      date: 'March 2022',
-      additionalContent: 'This is the second component.',
-    },
-    // Add more milestones as needed
-  ];
+
 
   const handleMouseLeave = () => {
     setHoveredIndex(null);
@@ -39,12 +28,12 @@ const Milestone: React.FC<MyComponentProps> = () => {
       {milestones.map((milestone, index) => (
       <li key={index} onMouseEnter={() => handleMouseEnter(index)} onMouseLeave={handleMouseLeave} className="mb-10 ms-4" >
           <div 
-            className={`absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700 ${
+            className={`absolute w-3 h-3 ${milestone.isCompleted ? 'bg-green-500' : 'bg-red-600'} rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700 ${
               hoveredIndex === index ? 'hovered' : ''
             }`}
           ></div>
           <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-            Application UI code in Tailwind CSS
+          {milestone.heading}
           </h3>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             February 2022
@@ -54,7 +43,24 @@ const Milestone: React.FC<MyComponentProps> = () => {
           {hoveredIndex === index && (
             <div className="absolute top-0 left-1/2 p-4 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-md shadow-md z-50">
               {/* Additional Component content */}
-              <p>{milestone.additionalContent}</p>
+              
+              <ul>
+                  {milestone.subMilestones.map((subMilestone, subIndex) => (
+                    
+<ol className="relative border-s border-gray-200 dark:border-gray-700">                  
+<li className="mb-10 ms-4">
+        <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{subMilestone.heading}</h3>
+        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{subMilestone.isCompleted ? `Completed`: `Not Completed`}</time>
+
+    </li>
+ 
+   
+</ol>
+
+
+                  ))}
+                </ul>
             </div>
           )}
         </li> ))}
