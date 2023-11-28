e30c6f94a9143ff6e254d721b60d300253c9ff8c
Varun-Kolanu <kolanuvarun739@gmail.com>
2023-11-28 18:29:37 +0530

    Routes update

--- src/app/api/project/route.js
+++ src/app/api/project/route.js
@@ -2,37 +2,32 @@
 import connectDb from "../../../../middlewares/mongoose";
 import Project from "../../../../models/Project";
 
-const handler = async (req, res) => {
-  if (req.method === 'POST') {
-    try {
-      const project = new Project(req.body);
-      const savedProject = await project.save();
-      res.status(201).json(savedProject);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error creating project' });
-    }
-  } else if (req.method === 'GET') {
-    try {
-      const projects = await Project.find({}, '-_id -__v')
-        .populate({
-          path: 'assignedTeam',
-          select: '-_id -__v',
-          populate: {
-            path: 'users',
-            select: '-_id -__v'
-          }
-        })
-        .populate('assignedBy', '-_id -__v');
-
-      res.status(200).json(projects);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error retrieving projects' });
-    }
-  } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
+export const POST = connectDb(async (req, res) => {
+  try {
+    const project = new Project(req.body);
+    const savedProject = await project.save();
+    res.status(201).json(savedProject);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error creating project' });
   }
-};
+});
 
-export default connectDb(handler);
+export const GET = connectDb(async (req, res) => {
+  try {
+    const projects = await Project.find({}, '-_id -__v')
+      .populate({
+        path: 'assignedTeam',
+        select: '-_id -__v',
+        populate: {
+          path: 'users',
+          select: '-_id -__v'
+        }
+      })
+      .populate('assignedBy', '-_id -__v');
+    res.status(200).json(projects);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error retrieving projects' });
+  }
+});
--- src/app/api/project/userprojects/route.js
+++ src/app/api/project/userprojects/route.js
@@ -2,34 +2,29 @@
 import connectDb from "../../../../../middlewares/mongoose";
 import Project from "../../../../../models/Project";
 
-const handler = async (req, res) => {
-  if (req.method === 'GET') {
-    const { userId } = req.query;
+export const GET = connectDb(async (req, res) => {
+  const { userId } = req.query;
 
-    if (!userId) {
-      res.status(400).json({ error: 'User ID is required' });
-      return;
-    }
-
-    try {
-      const projects = await Project.find({ 'assignedTeam.users': userId }, '-_id -__v')
-        .populate({
-          path: 'assignedTeam',
-          select: '-_id -__v',
-          populate: {
-            path: 'users',
-            select: '-_id -__v'
-          }
-        });
-
-      res.status(200).json(projects);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error retrieving projects for the user' });
-    }
-  } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
+  if (!userId) {
+    res.status(400).json({ error: 'User ID is required' });
+    return;
   }
-};
 
-export default connectDb(handler);
+  try {
+    const projects = await Project.find({ 'assignedTeam.users': userId }, '-_id -__v')
+      .populate({
+        path: 'assignedTeam',
+        select: '-_id -__v',
+        populate: {
+          path: 'users',
+          select: '-_id -__v'
+        }
+      });
+
+    res.status(200).json(projects);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error retrieving projects for the user' });
+  }
+});
+
