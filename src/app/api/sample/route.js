b9c6d891985342c8b15e4efe1ee2be9bcb533607
Varun-Kolanu <kolanuvarun739@gmail.com>
2023-11-28 20:19:41 +0530

    Updated Folder structure

--- src/app/api/sample/route.js
+++
@@ -1,8 +0,0 @@
-import connectDb from "../../../../middlewares/mongoose"
-// import Project from "../../../../models/Project";
-
-const handler = (req, res) => {
-    res.json({user: req.user})
-};
-
-export const GET = connectDb(handler);
--- src/pages/api/user/hello.js
+++
@@ -1,6 +0,0 @@
-// pages/api/hello.js
-export default function handler(req, res) {
-    // This API endpoint responds with a JSON object
-    res.status(200).json({ message: 'Hello, this is a basic API in Next.js!' });
-  }
-  
