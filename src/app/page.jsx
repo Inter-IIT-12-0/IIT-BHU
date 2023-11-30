c9adcaa423a95a1f05a089a38c2eae14abec0488
Roaster05 <100672872+Roaster05@users.noreply.github.com>
2023-11-30 18:36:24 +0530

    REVAMP workspace

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,6 +13,10 @@ import ProjectList from "../components/ProjectCard";
 import ProjectCard from "../components/MyProjectCard";
 import Calendar from "../components/Calendar";
 import TaskList from "../components/TaskList";
+import SubMilestoneCard from "../components/SubMilestoneCard";
+import SubMilestoneSidebar from "../components/SubMilestoneSidebar";
+import ProjectDashboard from "../components/ProjectDashboard";
+import RoundedProgressBar from "../components/RoundedProgressBar";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -31,6 +35,7 @@ export default function Home() {
       {/* <Calendar /> */}
 
 {/*       <Clickup /> */}
+<ProjectDashboard/>
     </main>
   );
 }
---
+++ src/components/SubMilestoneCard.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+
+const SubMilestoneCard = () => {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    setSelectedFiles((prevFiles) => [...prevFiles, ...files]);
+  };
+
+  const getInitialLetter = (fileName) => {
+    return fileName.charAt(0).toUpperCase();
+  };
+
+  return (
+    <div className="container mx-auto p-8">
+      {/* Main Heading and Text Options */}
+      <div className="flex justify-between items-center mb-4">
+      <h1 class="text-black font-Lato text-5xl font-medium leading-normal tracking-wide">Your Heading Text</h1>
+        <div className="flex space-x-4">
+        <p class="text-black font-Lato text-2xl font-semibold leading-normal tracking-tight underline">Team Chat</p>
+        <p class="text-black font-Lato text-2xl font-semibold leading-normal tracking-tight underline">Show Milestone</p>
+        </div>
+      </div>
+
+      {/* Text Description */}
+      <h1 class="text-black font-Lato text-2xl font-medium leading-normal tracking-tight" style={{"letterSpacing":0.7}}>Description</h1>
+      <p className="text-gray-700 font-Lato text-2xl font-medium leading-normal tracking-tight" style={{ letterSpacing: '0.7px' }}>
+      Quia voluptatem accusantium sint. Id sed possimus impedit. Corruapti qui soluta aperiam ullamquisquam aut pariatur ut. Dolores eum at doloribus eum quis rerum aspernatur porro. Nostrum aest ut illo neque.
+    </p>
+
+    <h1 class="text-black font-Lato text-2xl font-medium leading-normal tracking-tight" style={{"letterSpacing":0.7}}>Dates</h1>
+      <p className="text-gray-700 font-Lato text-2xl font-medium leading-normal tracking-tight" style={{ letterSpacing: '0.7px' }}>
+      12/11/23 - 20/11/23
+    </p>
+
+    <h1 class="text-black font-Lato text-2xl font-medium leading-normal tracking-tight" style={{"letterSpacing":0.7}}>CONNECTED APPS</h1>
+      {/* Cards */}
+      <div className="flex space-x-4 ">
+        {[1, 2, 3].map((index) => (
+          <div key={index} className="flex-shrink-0 w-1/3 bg-gray-200 p-4" style={{"height":"200px", "width":"200px", borderRadius:28}}>
+            {/* Card Image */}
+            <img
+              src="https://tse1.mm.bing.net/th?id=OIP.NI94miDnt29EtYfOyUGBUQHaLG&pid=Api&rs=1&c=1&qlt=95&w=76&h=115"
+              alt={`Image${index}`}
+              className="mb-4"
+            />
+            {/* Card Content */}
+            <p>Card {index} Content</p>
+          </div>
+        ))}
+      </div>
+
+      {/* File Upload */}
+      <h1 class="text-black font-Lato text-2xl font-medium leading-normal tracking-tight" style={{"letterSpacing":0.7}}>UPLOAD WORK</h1>
+      <div className="mt-8">
+        <input
+        id="fileInput"
+          type="file"
+          multiple
+          onChange={handleFileChange}
+          className="hidden"
+        />
+        
+
+
+        {/* Display Uploaded File Names in Flex-Row Container */}
+        { (
+            
+          <div className="mt-4 border p-2 flex flex-row">
+            <label htmlFor="fileInput" className="cursor-pointer border p-2">
+            <img src="https://img.icons8.com/external-anggara-basic-outline-anggara-putra/24/external-upload-button-user-interface-anggara-basic-outline-anggara-putra.png" alt="external-upload-button-user-interface-anggara-basic-outline-anggara-putra"></img>{/* Adjust size based on your icon */}
+          </label>
+            {selectedFiles.map((file, index) => (
+              <div key={index} className="mr-4">
+                {file.name}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SubMilestoneCard;
