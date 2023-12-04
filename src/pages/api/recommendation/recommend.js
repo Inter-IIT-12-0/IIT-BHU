d0599e9b4f42c145d7f38fd0a975c2e2d5592baf
atharvabhatt200 <atharvabhatt200@gmail.com>
2023-12-04 15:40:15 +0530

    add create sub-milestones feature

--- src/pages/api/recommendation/recommend.js
+++ src/pages/api/recommendation/recommend.js
@@ -1,20 +1,11 @@
 import axios from "axios";
 const API_URL = "https://api.openai.com/v1/chat/completions";
 const API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-// // Usage
-// getAllUniqueKeywords()
-//   .then((result)= > {
-// console.log(result)
-//})
-//   .catch((error)= > {
-// console.error(error)
-// })
+
 async function generateMessages(userPrompt) {
   const url = `/api/recommendation/userDetails/`;
-  console.log(url);
-  const response = await axios.get(url)
-  const listOfWords = response.data
-  console.log(listOfWords);
+  const response = await axios.get(url);
+  const listOfWords = response.data;
   const messages = [
     {
       role: "system",
@@ -25,7 +16,6 @@ async function generateMessages(userPrompt) {
     },
     { role: "user", content: userPrompt },
   ];
-  // console.log(messages);
   return messages;
 }
 
@@ -53,21 +43,21 @@ export async function GPT(messages) {
 
     // Read the response as a stream of data
     const res_message = await response.json();
-    console.log("response",res_message);
     const message = res_message["choices"][0]["message"]["content"];
-    return JSON.parse(message);
+    const signal = res_message["choices"][0]["finish_reason"];
+    if (signal !== "length") {
+      console.log(message);
+      return JSON.parse(message);
+    }
+    return { Error: "Token limit insufficient." };
   } catch (error) {
     // Handle fetch request errors
   }
 }
 
 export default async function recommend(statement) {
-  const messages = await generateMessages(
-    statement
-  );
+  const messages = await generateMessages(statement);
   // console.log(messages);
   const json_response = await GPT(messages);
   return json_response;
-  // return json_response;
-  // "I am currently in search of a talented frontend developer to join our dynamic team. The ideal candidate should possess a strong proficiency in HTML, CSS, and JavaScript, with a keen eye for detail in crafting visually appealing and user-friendly interfaces. You will play a crucial role in translating design mockups into responsive and interactive web applications. Experience with modern frontend frameworks such as React, Angular, or next.js is highly desirable. As a frontend developer, you will collaborate closely with our design and backend teams to ensure seamless integration and optimal user experiences."
 }
