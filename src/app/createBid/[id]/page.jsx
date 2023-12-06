a5a7e3c0a87c0adfd7e58ad10af673d58e4171f0
Roaster05 <100672872+Roaster05@users.noreply.github.com>
2023-12-06 11:09:46 +0530

    new

--- src/app/createBid/[id]/page.jsx
+++ src/app/createBid/[id]/page.jsx
@@ -12,6 +12,7 @@ import { simpleSearch } from '../../../lib/SearchAlgo'
 import FriendsSearchPopup from '../../../components/FriendsSearchPopup'
 import BackArrow_Icon from "../../../../public/Images/BackArrow_Icon.svg"
 import Folder_Icon from "../../../../public/Images/Folder_Icon.svg"
+import recommend from '../../../pages/api/recommendation/recommend'
 
 const CreateBid = ({ params }) => {
     // const num = 0
@@ -20,7 +21,7 @@ const CreateBid = ({ params }) => {
     const { data: session } = useSession();
     const [filter, setFilter] = useState('Past')
     const [pastTeamMembers, setPastTeamMembers] = useState([])
-    const [recommended, setRecommended] = useState()
+    const [recommended, setRecommended] = useState([])
     const [presentTeam, setPresentTeam] = useState({})
     const [teamName, setTeamName] = useState("")
     const [noOfTeams, setNoOfTeams] = useState(1)
@@ -46,13 +47,13 @@ const CreateBid = ({ params }) => {
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         let prevMilestones = [...milestones]
-        prevMilestones[selectedMilestone-1][name] = value
+        prevMilestones[selectedMilestone - 1][name] = value
         setMilestones(prevMilestones);
     };
     const handleFileChange2 = (e) => {
         const files = Array.from(e.target.files);
         let prevMilestones = milestones
-        prevMilestones[selectedMilestone-1].files = files
+        prevMilestones[selectedMilestone - 1].files = files
         setMilestones(prevMilestones);
     };
 
@@ -85,7 +86,7 @@ const CreateBid = ({ params }) => {
         let proposal = {
             proposalScore: 0,
             acceptanceProbability: 78,
-            bidAmount: milestones.reduce((acc, milestone) => acc + milestone.cost,0),
+            bidAmount: milestones.reduce((acc, milestone) => acc + milestone.cost, 0),
             startDate: new Date(startDate),
             milestones: modifiedMilestones
         }
@@ -124,16 +125,43 @@ const CreateBid = ({ params }) => {
                 })
                 setNonApprovals(nonApprov);
                 setPresentTeam(res.data)
-                setTeamName( res.data.teamName ? res.data.teamName : '' )
+                setTeamName(res.data.teamName ? res.data.teamName : '')
             }).catch(console.log)
 
-        axios.get('/api/allusers')
-            .then(res => {
-                setAllUsers(res.data)
-            }).catch(console.log)
+        
+        const fetchProject = async () => {
+            try {
+                const res1 = await axios.get(`/api/project/${id}`)
+                const res2 = await axios.get('/api/allusers')
+                
+                setProject(res1.data);
+                setAllUsers(res2.data);
+                fetchFilter(res1.data, res2.data);
+            }
+            catch {
+                console.error("Error fetching data");
+            }
+        }
+        fetchProject();
 
-        axios.get(`/api/project/${id}`).then(res => setProject(res.data))
-        axios.get('/api/allusers').then(res => setRecommended(res.data)).catch(console.log)
+        const fetchFilter = async (project, recommended) => {
+            try {
+                console.log(project, recommended)
+                if (project && recommended) {
+                    console.log(project.statement);
+                    const obj = await recommend(project.statement);
+                    setRecommended(recommended.filter(person => obj.includes(person.domain)));
+                }
+
+            }
+            catch (error) {
+                console.log("Error", error);
+            }
+        }
+
+        // fetchFilter();
+
+
     }, [noOfTeams, milestones])
 
     const addToTeam = (member) => {
@@ -364,21 +392,21 @@ const CreateBid = ({ params }) => {
                                     </div>
                                     <div className='px-6 mt-4 font-semibold flex'>
                                         <span className='mx-4'>Start Date <span className='text-red-500'> * </span> </span>
-                                        <input type="date" className='outline-none px-2 border-2 border-zinc-400 rounded-lg' required value={startDate} onChange={(e) => setStartDate(e.target.value)}/>
+                                        <input type="date" className='outline-none px-2 border-2 border-zinc-400 rounded-lg' required value={startDate} onChange={(e) => setStartDate(e.target.value)} />
                                     </div>
                                     <div className='flex flex-col w-full h-full px-6 my-3'>
                                         <div className='flex'>
                                             {
                                                 milestones.map((milestone, index) => (
-                                                    <div key={index} className='pb-2 flex items-center' onClick={() => setSelectedMilestone(index+1)}>
+                                                    <div key={index} className='pb-2 flex items-center' onClick={() => setSelectedMilestone(index + 1)}>
                                                         <span className={`h-full pt-2 cursor-pointer mx-3 px-3 ${selectedMilestone === index + 1 && 'bg-indigo-50'} `}>
-                                                Milestone {index + 1} {
-                                                    index === 0 && <span className='text-red-500'> * </span>
-                                                }
-                                                {/* {
+                                                            Milestone {index + 1} {
+                                                                index === 0 && <span className='text-red-500'> * </span>
+                                                            }
+                                                            {/* {
                                                     index === milestones.length - 1 && index !== 0 && <span className='bg-gray-300 rounded-full px-1' onClick={removeMilesone}> x </span>
                                                 } */}
-                                            </span>
+                                                        </span>
                                                     </div>
                                                 ))
                                             }
@@ -386,12 +414,12 @@ const CreateBid = ({ params }) => {
                                         </div>
 
 
-                                        
-                                                <div className='h-full bg-indigo-50 flex flex-col pb-8 max-h-72 overflow-scroll overflow-y-auto overflow-x-hidden'>
-                                                    <div className='flex w-full pt-4'>
-                                                        <input type="number" name='cost' value={milestones[selectedMilestone-1]['cost']} onChange={handleInputChange} placeholder='Expected cost &#8377;' className='mx-3 w-32 py-1 px-1 italic rounded-lg outline-none' required/>
-                                                        <input name='duration' value={milestones[selectedMilestone-1]['duration']} onChange={handleInputChange} type="number" placeholder='Enter duration' className='mx-3 w-32 py-1 px-1 italic rounded-lg outline-none' required/> <span className='-ml-3'> Weeks </span>
-                                                        {/* <div className='flex bg-white p-2 text-neutral-600 rounded-lg'>
+
+                                        <div className='h-full bg-indigo-50 flex flex-col pb-8 max-h-72 overflow-scroll overflow-y-auto overflow-x-hidden'>
+                                            <div className='flex w-full pt-4'>
+                                                <input type="number" name='cost' value={milestones[selectedMilestone - 1]['cost']} onChange={handleInputChange} placeholder='Expected cost &#8377;' className='mx-3 w-32 py-1 px-1 italic rounded-lg outline-none' required />
+                                                <input name='duration' value={milestones[selectedMilestone - 1]['duration']} onChange={handleInputChange} type="number" placeholder='Enter duration' className='mx-3 w-32 py-1 px-1 italic rounded-lg outline-none' required /> <span className='-ml-3'> Weeks </span>
+                                                {/* <div className='flex bg-white p-2 text-neutral-600 rounded-lg'>
                                                             <Folder_Icon className="mr-1" />
                                                             {
                                                                 milestones[selectedMilestone-1].files.length !== 0 ?
@@ -410,23 +438,23 @@ const CreateBid = ({ params }) => {
                                                             }
                                                         </div> */}
 
-                                                    </div>
-                                                    <div className='flex flex-col px-6 my-4 '>
-                                                        <div className='flex items-center'>
-                                                            <span>Milestone Title</span>
-                                                            <input name='title' value={milestones[selectedMilestone-1]['title']} onChange={handleInputChange} type="text" placeholder='Enter Milestone title' className='w-2/3 mx-10 my-4 h-8 px-4 rounded-xl py-6 outline-none' required/>
-                                                        </div>
-                                                        <div className='flex items-center'>
-                                                            <span>Description</span>
-                                                            <input name='description' value={milestones[selectedMilestone-1]['description']} onChange={handleInputChange} type="text" placeholder='Enter Description' className='w-2/3 mx-10 my-4 h-8 px-4 rounded-xl py-9 outline-none'/>
-                                                        </div>
-                                                        <div className='flex items-center'>
-                                                            <span>Deliverables</span>
-                                                            <input name='deliverables' value={milestones[selectedMilestone-1]['deliverables']} onChange={handleInputChange} type="text" placeholder='Enter Deliverables' className='w-2/3 mx-10 my-4 h-8 px-4 rounded-xl py-9 outline-none' required />
-                                                        </div>
-                                                    </div>
-
+                                            </div>
+                                            <div className='flex flex-col px-6 my-4 '>
+                                                <div className='flex items-center'>
+                                                    <span>Milestone Title</span>
+                                                    <input name='title' value={milestones[selectedMilestone - 1]['title']} onChange={handleInputChange} type="text" placeholder='Enter Milestone title' className='w-2/3 mx-10 my-4 h-8 px-4 rounded-xl py-6 outline-none' required />
                                                 </div>
+                                                <div className='flex items-center'>
+                                                    <span>Description</span>
+                                                    <input name='description' value={milestones[selectedMilestone - 1]['description']} onChange={handleInputChange} type="text" placeholder='Enter Description' className='w-2/3 mx-10 my-4 h-8 px-4 rounded-xl py-9 outline-none' />
+                                                </div>
+                                                <div className='flex items-center'>
+                                                    <span>Deliverables</span>
+                                                    <input name='deliverables' value={milestones[selectedMilestone - 1]['deliverables']} onChange={handleInputChange} type="text" placeholder='Enter Deliverables' className='w-2/3 mx-10 my-4 h-8 px-4 rounded-xl py-9 outline-none' required />
+                                                </div>
+                                            </div>
+
+                                        </div>
 
                                         <div className='w-full flex justify-end px-10 text-white'>
                                             <button className='bg-sky-700 px-2 py-3 rounded-3xl w-40 text-xl' onClick={handleSubmit} >Submit</button>
